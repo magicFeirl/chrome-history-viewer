91bd4c3f3a28eabaff46a11ba352e274b1aa0f85
magicFeirl <2100709458@qq.com>
2025-10-14 15:58:21 +0800

    feat: 小优化

--- html/js/account.js
+++ html/js/account.js
@@ -1,3 +1,11 @@
-export const disableAccount = () => {
+export const liveCheck = (emailList) => {
+    const emailListStr = emailList.join('\n\n')
+    console.info(emailListStr)
 
+    navigator.clipboard.writeText(emailListStr).then(() => {
+        window.open('https://colab.research.google.com/drive/1O2gn4gCyGGFmo1aL97rB8L7ZozmGw1qL', '_blank')
+    }).catch((e) => {
+        alert('写入剪贴板失败，请手动在控制台复制邮箱')
+        console.error(e)
+    })
 }
--- html/js/chrome.js
+++ html/js/chrome.js
@@ -1,4 +1,5 @@
 import { request, BASE_URL } from './http.js'
+import { ROW_STATE } from './row.js'
 
 export const openChrome = async (browser, link) => {
     // if (link.state_type === ROW_STATE.unknown) {
@@ -12,20 +13,20 @@ export const openChrome = async (browser, link) => {
     const result = await resp.json();
 };
 
-export const openChromeOneByOne = async () => {
+export const openChromeOneByOne = async (data, openLink, openCount) => {
     const autoOpen = async () => {
-        let openCount = 0;
-        const openRows = data.value.filter((item) =>
+        let count = 0;
+        const openRows = data.filter((item) =>
             [ROW_STATE.available, ROW_STATE.unknown].includes(
                 item.lastLink.state_type
             )
         );
-
+        console.log(openRows)
         for (const { browser } of openRows) {
-            openCount++;
-            await openChrome(browser, { url: state.value.openLink });
+            count++;
+            await openChrome(browser, { url: openLink });
 
-            if (openCount >= state.value.openCount) {
+            if (count >= openCount) {
                 break;
             }
         }
@@ -33,11 +34,11 @@ export const openChromeOneByOne = async () => {
 
     const selectOpen = async (rows) => {
         for (const { browser } of rows) {
-            await openChrome(browser, { url: state.value.openLink });
+            await openChrome(browser, { url: openLink });
         }
     };
 
-    const selectRows = data.value.filter((item) => item.checked);
+    const selectRows = data.filter((item) => item.checked);
     // 有勾选打开则使用勾选的 Profile
     if (selectRows.length) {
         selectOpen(selectRows);
